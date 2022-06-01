45096bf384dd1fc4564a22e6c0abb247a9309366
Jakhongir Kayumov <jakhongir.dev@gmail.com>
2022-06-01 05:44:07 +0500

    Add partners section

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -8,3 +8,14 @@ var swiperServices = new Swiper(".swiperServices", {
     prevEl: ".services__slider-prev-btn",
   },
 });
+
+var swiperServices = new Swiper(".swiperStaff", {
+  loop: true,
+  autoplay: true,
+  slidesPerView: 3,
+  spaceBetween: 60,
+  navigation: {
+    nextEl: ".services__slider-next-btn",
+    prevEl: ".services__slider-prev-btn",
+  },
+});
