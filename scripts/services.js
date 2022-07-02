b475e410362ced31a89790e215d35fa76367d040
code-ness <jakhongir.dev@gmail.com>
2022-07-03 02:01:16 +0500

    Tablet

--- scripts/services.js
+++ scripts/services.js
@@ -3,10 +3,6 @@ var showLessBtn = document.querySelector(".btn-show-less-js");
 var moreServices = document.querySelector(".services-more-js");
 var services = document.getElementById("services");
 
-var position = services.getBoundingClientRect();
-var x = position.x;
-var y = position.y + window.innerHeight;
-
 showMoreBtn.addEventListener("click", () => {
   moreServices.classList.remove("hidden");
   showMoreBtn.style.display = "none";
@@ -17,5 +13,5 @@ showLessBtn.addEventListener("click", () => {
   moreServices.classList.add("hidden");
   showMoreBtn.style.display = "block";
   showLessBtn.style.display = "none";
-  scrollTo(x, y);
+  services.scrollIntoView(3);
 });
--- scripts/swiper.js
+++ scripts/swiper.js
@@ -3,12 +3,23 @@ var sliders = document.querySelectorAll(".staff__box");
 var swiperServices = new Swiper(".swiperStaff", {
   loop: true,
   autoplay: true,
-  slidesPerView: 3,
-  spaceBetween: 60,
+  slidesPerView: 1,
+  spaceBetween: 20,
   navigation: {
     nextEl: ".staff-next-btn",
     prevEl: ".staff-prev-btn",
   },
+  breakpoints: {
+    768: {
+      autoplay: true,
+      slidesPerView: 2
+    },
+    1050: {
+      autoplay: true,
+      spaceBetween: 40,
+      slidesPerView: 3
+    },
+  }
 });
 
 sliders.forEach(item => {
